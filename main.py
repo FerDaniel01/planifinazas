1ec42b359b4b1f5c13523c9d0ed83f6a66db5a9d
FerDaniel01 <fernando.drinconruiz@gmail.com>
2020-12-17 22:37:47 -0500

    back con url heroku

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 import db
 
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+
+    "http://localhost:8081",
+    "https://planifinanzas-front.herokuapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 
 @app.get("/transacciones/")
